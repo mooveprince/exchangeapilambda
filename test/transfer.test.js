56479e075e5a402d7722c43a910e7ebfcb48dbf2
Moovendhan Elanchezhiyan <moove@Moovendhans-MacBook-Pro.local>
2020-01-18 16:56:56 -0500

    Test case

--- test/transfer.test.js
+++ test/transfer.test.js
@@ -1,6 +1,17 @@
 var expect = require( 'chai' ).expect;
 var LambdaTester = require( 'lambda-tester' ).noVersionCheck();
 var transferApi = require( '../transfer-from-usa' ).getRate;
+var transferCanadaApi = require( '../transfer-from-canada' ).getRate;
+
+describe('transferCanadaApi', function() {
+    it( 'test success', function() {
+		return LambdaTester( transferCanadaApi )
+			.event()
+			.expectResult((result) => {
+                expect( result.statusCode).to.be.equal(200);
+            });
+	});
+});
 
 describe('transferApi', function() {
     it( 'test success', function() {
